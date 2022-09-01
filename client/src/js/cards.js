6cbb9ee14ef4efa1d4e1eab6a99d6e3866a07bd1
jessica-calderon <calderonjessica13@yahoo.com>
2022-09-01 01:31:22 -0500

    add delete card functionality

--- client/src/js/cards.js
+++ client/src/js/cards.js
@@ -21,7 +21,7 @@ export const fetchCards = async () => {
             ${data.name} 
             </div>
           <div class="card-body">
-          <img src="${Bear}" class="card-img-top" alt="...">
+          <img src="${Bear}" width="50px" class="card-img-top" alt="...">
           <h5 class="card-title" data-email="${data.email}">${data.email}</h5>
           <h5 class="card-title" data-phone="${data.phone}">${data.phone}</h5>
           <button class="btn btn-danger" data-id="${data.id}" data-name="${data.name}" data-phone="${data.phone}" data-email= "${data.email}" onclick="editCard(this)" >Edit</button>
@@ -36,7 +36,7 @@ export const fetchCards = async () => {
             ${data.name} 
             </div>
           <div class="card-body">
-          <img src="${Dog}" class="card-img-top" alt="...">
+          <img src="${Dog}" width="50px" class="card-img-top" alt="...">
           <h5 class="card-title" data-email="${data.email}">${data.email}</h5>
           <h5 class="card-title" data-phone="${data.phone}">${data.phone}</h5>
           <button class="btn btn-danger" data-id="${data.id}" data-name="${data.name}" data-phone="${data.phone}" data-email= "${data.email}" onclick="editCard(this)" >Edit</button>
--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,10 +1,8 @@
-// import  "./form";
-
 import Logo from '../images/logo.png';
 import Bear from '../images/bear.png';
 import Dog from '../images/dog.png';
 
-import { initdb, getDb, postDb } from './database';
+import { initdb, getDb, postDb, deleteDb } from './database';
 import { fetchCards } from './cards';
 import { toggleForm, clearForm } from './form';
 
@@ -51,3 +49,12 @@ window.addEventListener('load', function() {
   // Reload the DOM
   fetchCards();
   });
+  
+  window.deleteCard = (e) => {
+    // Grabs the id from the button element attached to the contact card.
+    let id = parseInt(e.id);
+    // Delete the card
+    deleteDb(id);
+    // Reload the DOM
+    fetchCards();
+  };
